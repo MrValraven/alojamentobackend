be0b11acc18e06ff8ce43c4f067866cfd1ee51bb
mrvalraven <akatiago@gmail.com>
2022-08-06 19:04:39 +0100

    added users endpoint

---
+++ controllers/usersController.js
@@ -0,0 +1,17 @@
+const User = require("../model/User");
+
+const getAllUsers = async (request, response) => {
+  let usersInDatabase = await User.find({});
+
+  if (!usersInDatabase) {
+    return res.status(400).json({ message: "No users available" });
+  }
+  usersInDatabase = usersInDatabase.map((user) => ({
+    email: user.email,
+    roles: user.roles,
+  }));
+  console.log("users:", usersInDatabase);
+  response.json(usersInDatabase);
+};
+
+module.exports = { getAllUsers };
