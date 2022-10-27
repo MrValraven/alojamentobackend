2af924b988ed6213d81d6c58a75f56f0e2b22176
DiogoSolipa <diogopaissolipa@gmail.com>
2022-10-27 18:20:11 +0100

    added CORS for SIIUE

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "https://www.alojamentoasap.vercel.app",
   "http://127.0.0.1:5173",
   "http://localhost:5173/",
+  "https://siiue.uevora.pt"
 ];
 
 module.exports = allowedOrigins;
